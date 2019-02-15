756a607ad267eed6dd2038544540414bfb124dde
morejust.store <store@morejust.store>
2019-02-15 20:31:12 +0000

    add index.php to 07d317eace624188c0ac4946746443bb branch

---
+++ index.php
@@ -0,0 +1,12 @@
+if ($handle = opendir('.')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
